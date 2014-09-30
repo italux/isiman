fa8c8b2a5fa80383bfbf188505f5b6bdbef46997
Italo Santos <okdokk@gmail.com>
2014-09-30 00:50:09 -0300

    Add support to OneFS 7.0 and 7.1 API

--- isiman/isiman/libs/isilon.py
+++ isiman/isiman/libs/isilon.py
@@ -1,90 +1,331 @@
-import re
-import tempfile
 from isiman.libs import utils
+import requests
+import re
 
-def makesshkey(key):
-    temp = tempfile.NamedTemporaryFile(delete=False)
-    f = open(temp.name,'w')
-    f.write(key)
-    f.close()
-    return temp.name
+def isi_protection_overhead(protection, nodes):
+    """Matrix to calculate OneFS protection overhead"""
 
-def isiremote(cluster, command):
-    address = cluster.address
-    username = cluster.username
-    private_key = makesshkey(cluster.ssh_key)
-    if username != 'root':
-        command = 'sudo ' + command
-    output = utils.remote_command(address, username, private_key, command)
-    return output
+    # The table below was based on "Isilon OneFS 7.0 Administration Guide"
+    # page 145, section "Data protection level disk space usage"
+    percent_overhead = {
+        "N+2:1": {
+            3: 33.33, 4: 25.00,
+            5: 20.00, 6: 16.67,
+            7: 14.29, 8: 12.50,
+            'default': 11.11
+        },
+        "N+3:1": {
+            4: 25.00, 5: 20.00,
+            6: 16.67, 7: 14.29,
+            8: 12.50, 'default': 11.11
+        },
+        "N+2": {
+            5: 40.00, 6: 33.33, 7: 28.57,
+            8: 25.00, 9: 22.22, 10: 20.00,
+            11: 18.18, 12: 16.67, 13: 15.38,
+            14: 14.29, 15: 13.33, 16: 12.50,
+            17: 11.76, 'default': 11.11
+        },
+        "N+3": {
+            7: 42.86, 8: 37.50, 9: 33.33,
+            10: 30.00, 11: 27.27, 12: 25.00,
+            13: 23.08, 14: 21.43, 15: 20.00,
+            16: 18.75, 17: 17.65, 18: 16.67,
+            'default': 15.79
+        }
+    }
+
+    # After a specifc number of nodes the protection overhead shall be constant,
+    # so we pass to use "default" percent protection overhead
+    if protection == "N+2:1" and nodes >= 9:
+        return percent_overhead.get(protection).get('default')
+    elif protection == "N+3:1" and nodes >= 9:
+        return percent_overhead.get(protection).get('default')
+    elif protection == "N+2" and nodes >= 18:
+        return percent_overhead.get(protection).get('default')
+    elif protection == "N+3" and nodes >= 19:
+        return percent_overhead.get(protection).get('default')
+    else:
+        return percent_overhead.get(protection).get(nodes)
+
+def isiremote(address, username, ssh_key, command):
+    """Remote intereaction with Isilon through SSH connection using private key.
+    We'll check if user was different from root we'll add "sudo" before the command"""
+
+    try:
+        private_key = utils.ssh_private_key(ssh_key)
+        if username != 'root':
+            command = 'sudo ' + command
+        output = utils.remote_command(address, username, private_key, command)
+        return output
+    except Exception, e:
+        raise(e)
+
+def version(cluster):
+    """Get OneFS release version through SSH using isiremote()"""
+
+    command = '/usr/bin/isi version osrelease'
+    try:
+        output = isiremote(cluster.address, cluster.username, cluster.ssh_key, command)
+        return output.strip().replace('v', '')
+    except Exception, e:
+        raise(e)
 
 def status(cluster):
+    """Get Isilon cluster status using SNMP"""
+
     oid = '.1.3.6.1.4.1.12124.1.1.2.0'
     statusmap = {0: "OK", 1: "ATTN", 2: "OFFLINE", 3: "INVALID" }
-    clusterstatus = utils.snmpget(cluster.address, oid)
-    return statusmap.get(int(clusterstatus))
+    try:
+        clusterstatus = utils.snmpget(cluster.address, oid)
+        return statusmap.get(int(clusterstatus))
+    except Exception, e:
+            raise(e)
 
 def cpu_usage(cluster):
+    """Get Isilon cluster CPU usage using SNMP"""
+
     oid = '.1.3.6.1.4.1.12124.1.2.3.5.0'
-    cpu_usage = utils.snmpget(cluster.address, oid)
-    return float(100 - float(int(cpu_usage) / 10))
+    try:
+        cpu_usage = utils.snmpget(cluster.address, oid)
+        return float(100 - float(int(cpu_usage) / 10))
+    except Exception, e:
+        raise(e)
 
 def nodes(cluster):
-    command = '/usr/bin/isi status -q -n | /usr/bin/egrep "Name|ID|LNN|Health" | /usr/bin/paste - - - - | /usr/bin/sed "s/Node //g"'
-    output = isiremote(cluster, command)
-    nodes = {}
-    nodeslist = [ line.split('\t') for line in output.split('\n') if len(line) > 0 ]
-    [ nodes.update({node[2].replace(" ", "").split(':')[1]: {}}) for node in nodeslist ]
-    [ nodes[node[2].replace(" ", "").split(':')[1]].update({node[0].replace(" ", "").lower().split(':')[0]:node[0].replace(" ", "").lower().split(':')[1], node[1].replace(" ", "").lower().split(':')[0]:node[1].replace(" ", "").lower().split(':')[1], node[3].replace(" ", "").lower().split(':')[0]:node[3].replace(" ", "").split(':')[1]}) for node in nodeslist ]
-    return nodes
+    """Get Isilon nodes basic information:
+    
+    Node LNN
+    Node ID
+    Node Name
+    IP Address
+    Health
+    Serial Number
+    """
+    command = '/usr/bin/isi status -q -n | /usr/bin/egrep "Name|ID|LNN|Health|Address|SN:" | /usr/bin/paste - - - - - - | /usr/bin/sed "s/Node //g"'
+    try:
+        output = isiremote(cluster.address, cluster.username, cluster.ssh_key, command)
+        nodes = {}
+        nodeslist = [ line.split('\t') for line in output.split('\n') if len(line) > 0 ]
+        [ nodes.update({node[2].replace(" ", "").split(':')[1]: {}}) for node in nodeslist ]
+        [ nodes[node[2].replace(" ", "").split(':')[1]].update({
+            node[0].replace(" ", "").lower().split(':')[0]:node[0].replace(" ", "").split(':')[1],
+            node[1].replace(" ", "").lower().split(':')[0]:node[1].replace(" ", "").split(':')[1],
+            node[3].replace(" ", "").lower().split(':')[0]:node[3].replace(" ", "").split(':')[1],
+            node[4].replace(" ", "").lower().split(':')[0]:node[4].replace(" ", "").split(':')[1],
+            node[5].replace(" ", "").lower().split(':')[0]:node[5].replace(" ", "").split(':')[1]
+            }) for node in nodeslist ]
+        return nodes
+    except Exception, e:
+        raise(e)
+
+def servicelight(node, action):
+    """Manage nodes servicelight through SSH using isiremote()"""
+
+    command = '/usr/bin/isi servicelight %s' % action
+    if action == "on" or action == "off":
+        isiremote(node.ipaddress, node.cluster.username, node.cluster.ssh_key, command)
+
+        # "isi" commands aways return status code 0 (zero), so after execute a command
+        # we need validate if the service light status was changed as we expected
+
+        statuscmd = '/usr/bin/isi servicelight status'
+        output = isiremote(node.ipaddress, node.cluster.username, node.cluster.ssh_key, statuscmd)
+
+        if output.split('\n')[0].split()[-1] == action:
+            return output.split('\n')[0].split()[-1]
+        else:
+            raise Exception('Error change servicelight status')
+    elif action == "status":
+        output = isiremote(node.ipaddress, node.cluster.username, node.cluster.ssh_key, command)
+        if output.split('\n')[0].split()[-1] == "on" or output.split('\n')[0].split()[-1] == "off":
+            return output.split('\n')[0].split()[-1]
+        else:
+            raise Exception('Error getting servicelight status')
+    else:
+        raise Exception('Command not found')
 
 def clients_connected_bynode(cluster):
+    """Get nodes statistics informations about clients connections through SSH using "isi statistics query" command"""
+
+    # We just get the clientstats.connected.[cifs, nfs]
+    # because is that same information that we can see on the isilon cluster WebUI
+
     command = '/usr/bin/isi statistics query --nodes=all --stats=node.clientstats.connected.cifs,node.clientstats.connected.nfs --noheader --nofooter'
-    output = isiremote(cluster, command)
-    connections = {}
-    connlist = [ line.split() for line in output.split('\n') if len(line) > 0 ]
-    [ connections.update({conn[0]:{"cifs":int(conn[1]), "nfs":int(conn[2]), "total": (int(conn[1])+int(conn[2]))}}) for conn in connlist ]
-    return connections
+    try:
+        output = isiremote(cluster.address, cluster.username, cluster.ssh_key, command)
+        connections = {}
+        connlist = [ line.split() for line in output.split('\n') if len(line) > 0 ]
+        [ connections.update({conn[0]:{"cifs":int(conn[1]), "nfs":int(conn[2]), "total": (int(conn[1])+int(conn[2]))}}) for conn in connlist ]
+        return connections
+    except Exception, e:
+        raise(e)
 
 def events(cluster):
-    command = '/usr/bin/isi events list --columns=id,lnn,severity,message -w --csv'
-    output = isiremote(cluster, command)
-    events = [ event.split(',') for event in output.split('\n') if len(event) > 0 ]
-    return events
+    """Get current cluster events through SSH using isiremote()"""
 
-def jobs(cluster):
-    command = '/usr/bin/isi job status -r'
-    output = isiremote(cluster, command)
-    regex=re.compile("jobs.\d.(type|policy|impact|priority|state|running_time)=.*")
-    jobs = {}
-    joblist = [ job.group() for line in output.split('\n') for job in [regex.search(line)] if job]
-    [ jobs.update({job.split(".")[1]: {}}) for job in joblist ]
-    [ jobs[job.split(".")[1]].update({job.split(".")[2].split("=")[0]: job.split("=")[-1]}) for job in joblist ]
-    return jobs
+    command = '/usr/bin/isi events list --columns=id,lnn,severity,message,start_time,end_time -w --csv'
+    try:
+        output = isiremote(cluster.address, cluster.username, cluster.ssh_key, command)
+        events = [ event.split(',', 5) for event in output.split('\n') if len(event) > 0 ]
+        return events
+    except Exception, e:
+        raise(e)
 
 def smartpools(cluster):
-    command = '/usr/bin/isi status -d -q | /usr/bin/egrep "\|" | /usr/bin/sed 1d'
-    output = isiremote(cluster, command)
-    pools = {}
-    [ pools.update({pool.split("|")[0].strip(): {}}) for pool in output.split("\n") if len(pool) > 0 ]
-    for pool in output.split("\n"):
-        if len(pool) > 0:
-            hdd_size = utils.human2bytes(pool.split("|")[5].split("/")[1].strip().split("(")[0])
-            hdd_used = utils.human2bytes(pool.split("|")[5].split("/")[0].strip())
-            if "No SSDs" in pool.split("|")[6].split("/")[0].strip():
-                ssd_size = 0
-                ssd_used = 0
-            else:
-                ssd_size = utils.human2bytes(pool.split("|")[6].split("/")[1].strip().split("(")[0])
-                ssd_used = utils.human2bytes(pool.split("|")[6].split("/")[0].strip())
+    """Get SmartPools created and the usage infotmation"""
 
-            pools[pool.split("|")[0].strip()].update({
-                'hdd': {
-                    'size': hdd_size, 
-                    'used': hdd_used
-                    },
-                'ssd': {
-                    'size': ssd_size, 
-                    'used': ssd_used 
-                    }})
-    return pools
+    command = '/usr/bin/isi status -d -q | /usr/bin/egrep "\|" | /usr/bin/sed 1d'
+    try:
+        output = isiremote(cluster.address, cluster.username, cluster.ssh_key, command)
+        pools = {}
+        [ pools.update({pool.split("|")[0].strip(): {}}) for pool in output.split("\n") if len(pool) > 0 ]
+        for pool in output.split("\n"):
+            if len(pool) > 0:
+                hdd_size = utils.human2bytes(pool.split("|")[5].split("/")[1].strip().split("(")[0])
+                hdd_used = utils.human2bytes(pool.split("|")[5].split("/")[0].strip())
+                if "No SSDs" in pool.split("|")[6].split("/")[0].strip():
+                    ssd_size = 0
+                    ssd_used = 0
+                else:
+                    ssd_size = utils.human2bytes(pool.split("|")[6].split("/")[1].strip().split("(")[0])
+                    ssd_used = utils.human2bytes(pool.split("|")[6].split("/")[0].strip())
+
+                pools[pool.split("|")[0].strip()].update({
+                    'hdd': {
+                        'size': hdd_size, 
+                        'used': hdd_used
+                        },
+                    'ssd': {
+                        'size': ssd_size, 
+                        'used': ssd_used 
+                        }})
+        return pools
+    except Exception, e:
+        raise(e)
+
+class RemoteSSH(object):
+    """Remote intereaction with Isilon through SSH connection using private key.
+    We'll check if user was different from root we'll add "sudo" before the command"""
+
+    def __init__(self, cluster):
+        super(RemoteSSH, self).__init__()
+        self.cluster = cluster
+
+    def get(self, resource):
+        """Get information from Isilon Cluster through SSH connection"""
+
+        if resource == '/platform/1/job/jobs':
+            __command = '/usr/bin/isi job status -r'
+
+        return self.__execute__(__command)
+
+    def __execute__(self, command):
+        try:
+            __private_key = utils.ssh_private_key(self.cluster.ssh_key)
+            if self.cluster.username != 'root':
+                command = 'sudo ' + command
+            output = utils.remote_command(self.cluster.address, self.cluster.username, __private_key, command)
+            return output
+        except Exception, e:
+            raise(e)
+
+class API(object):
+    """Intereaction with Isilon built-in API through HTTP Basic Auth"""
+
+    def __init__(self, cluster):
+        super(API, self).__init__()
+        self.cluster = cluster
+        
+    def get(self, resource):
+        """Get information from Isilon Cluster through API"""
+
+        __method = 'GET'
+        try:
+            return self.__api__(__method, resource)
+        except Exception, e:
+            raise(e)
+
+    def __api__(self, method, resource):
+        """Generic method to interact with Isilon Cluster through API"""
+
+        __timeout = 30
+        try:
+            __url = "https://" + self.cluster.address + ":8080" + resource
+            result = requests.request(method, __url, auth=(self.cluster.username, self.cluster.password), timeout=__timeout ,verify=False)
+            return result.json()
+        except Exception, e:
+            raise(e)
+
+class Jobs(object):
+    """Interact and Manage Jobs on Isilon cluster"""
+    
+    def __init__(self, cluster):
+        super(Jobs, self).__init__()
+        self.cluster = cluster
+        
+    def list(self):
+        """List current jobs on Isilon cluster"""
+
+        resource = '/platform/1/job/jobs'
+        try:
+            result = self.__load__(resource)
+            return result.get('jobs')
+        except Exception, e:
+            raise(e)
+
+    def __load__(self, resource):
+        
+        # Some "isi job" commands was changed on OneFS version 7.1 or later
+        # so we need check the cluster version before run the command.
+        # 
+        # The job status on OneFS version 7.1 will be obtained from Isilon API
+        # in older versions, like 6.5 and/or 7.0 we'll need use SSH to do that.
+        
+        if self.cluster.version.startswith('7.1'):
+            __api = API(self.cluster)
+            return __api.get(resource)
+        elif self.cluster.version.startswith('7.0') or self.cluster.version.startswith('6.5'):
+            __ssh = RemoteSSH(self.cluster)
+            __output = __ssh.get(resource)
+            __jobs  ={}
+            regex=re.compile("jobs.\d.(id|impact|description|phase_cur|state|priority|participants|create_time|policy|progress|retries|type|phase_total)=.*", re.MULTILINE|re.DOTALL)
+            rawjobs = [ job.group() for line in utils.split_jobs_output(__output) for job in [regex.search(line)] if job]
+            [ __jobs.update({job.split(".")[1]: {}}) for job in rawjobs ]
+            [ __jobs[job.split(".")[1]].update({job.split(".")[2].split("=")[0]: job.split("=")[-1].replace('\n', ' ')}) for job in rawjobs ]
+            return {'jobs': [ job for job in __jobs.itervalues() ] }
+        else:
+            raise Exception('OneFS version (%s) not supported.' % self.cluster.version)
+
+class Snapshots(object):
+    """Interact and Manage Snapshots on Isilon cluster"""
+    def __init__(self, arg):
+        super(Snapshots, self).__init__()
+        self.api = API(self.cluster)
+
+    def summary(self):
+        resource = '/platform/1/snapshot/snapshots-summary'
+        summary = self.__load__(self.cluster, resource).get('summary')
+        return summary
+
+    def list(self):
+        resource = '/platform/1/snapshot/snapshots'
+        snapshots = self.__load__(resource).get('snapshots')
+        return snapshots
+
+    def get(self, snap_id):
+        resource = '/platform/1/snapshot/snapshots/%s' % snap_id
+        snapshot = self.__load__(resource).get('snapshots')
+        return snapshot
+
+    def __load__(self, resource):
+        """Get Snapshots data from Isilon API"""
+
+        if self.cluster.version.startswith('7.'):
+            try:
+                result = self.api.get(resource)
+                return result
+            except Exception, e:
+                raise(e)
+        else:
+            raise Exception('OneFS version (%s) not supported.' % self.cluster.version)
--- isiman/isiman/libs/utils.py
+++ isiman/isiman/libs/utils.py
@@ -1,27 +1,65 @@
-import os, commands, paramiko
-# from isiman.models import Cluster
+import os, commands, syslog, tempfile
+import paramiko
+
+def log(message):
+    """Log message with specifc priority to LOG_DAEMON"""
+    syslog.openlog("isiman", syslog.LOG_PID, syslog.LOG_DAEMON)
+    syslog.syslog(syslog.LOG_INFO, message)
+    syslog.closelog()
+
+def uniq(seq):
+    """Unique items in a list"""
+    checked = []
+    for e in seq:
+       if e not in checked:
+           checked.append(e)
+    return checked
+
+def split_jobs_output(output):
+    """Split Isilon jobs output over SSH connection to remove break lines from jobs.* lines"""
+    result = []
+    for line in output.splitlines():
+        if (line.startswith("jobs.") and len(result) > 0):
+            yield("\n".join(result))
+            result = []
+        if (line.strip() != ""):
+            result.append(line)
+    if (len(result) > 0):
+        yield("\n".join(result))
 
 def which(command):
+    """Abstraction of posix command using on system PATH variable"""
     envpath=os.getenv('PATH')
     for path in envpath.split(os.path.pathsep):
         path=os.path.join(path,command)
         if os.path.exists(path) and os.access(path,os.X_OK):
             return path
 
-def snmpget(host, oid, version="v2c", community="public"):
-    try:
-        snmpget = which('snmpget')
-        command = '%s -%s -c %s %s %s' % (snmpget, version, community, host, oid)
-        return commands.getoutput(command).split()[-1].strip('""')
-    except Exception, e:
-        return e
+def snmpget(host, oid, version="v2c", community="public", timeout=30):
+    """Abstraction of "snmpget" posix command"""
+    snmpget = which('snmpget')
+    command = '%s -%s -c %s -t %s %s %s' % (snmpget, version, community, timeout, host, oid)
+    status, output = commands.getstatusoutput(command)
+    if status == 0:
+        # remove quotation marks from snmp result value
+        return output.split()[-1].strip('""')
+    else:
+        raise Exception("%s" % output)
 
-def remote_command(host, username, private_key, command):
-    """Executes a remote command over ssh connection using ssh key to auth."""
+def ssh_private_key(key):
+    """Create a temporary ssh private key file"""
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    f = open(temp.name,'w')
+    f.write(key)
+    f.close()
+    return temp.name
+
+def remote_command(host, username, private_key, command, timeout=30):
+    """Executes a remote command over ssh connection using private ssh key"""
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, username=username, key_filename=private_key)
+        ssh.connect(host, username=username, key_filename=private_key, timeout=timeout)
         stdin, stdout, stderr = ssh.exec_command(command)
         return stdout.read()
     except Exception, e:
@@ -30,6 +68,7 @@ def remote_command(host, username, private_key, command):
         ssh.close()
 
 def human2bytes(size):
+    """Convert human binary prefix to bytes"""
     SYMBOLS = {
         'customary'     : ('B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'),
         'customary_ext' : ('byte', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa',
@@ -56,7 +95,7 @@ def human2bytes(size):
                 sset = SYMBOLS['customary']
                 letter = letter.upper()
             else:
-                raise ValueError("can't interpret %r" % init)
+                raise ValueError("Can't interpret %r" % init)
         prefix = {sset[0]:1}
         for i, s in enumerate(sset[1:]):
             prefix[s] = 1 << (i+1)*10
